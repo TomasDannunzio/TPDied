9767988fb1d723c30840b7c6617bdb3a44357447
TomasDannunzio <tomidannunzio@gmail.com>
2023-07-27 20:09:22 -0300

    Más sentencias SQL, y agregado de Singleton en Gestor. Solucionar conexion base de datos

--- src/Gestores/GestorRuta.java
+++ src/Gestores/GestorRuta.java
@@ -12,15 +12,15 @@ public class GestorRuta {
             ResultSet resultSet = statement.executeQuery(
                 "select * from ruta where ruta_id ="+i+"");
        
-            while (resultSet.next()) {
+            /*while (resultSet.next()) {
                r = new Ruta(resultSet.getInt("ruta_id"),
-            		   resultSet.getInt"origen"),
+            		   resultSet.getInt("origen"),
             		   resultSet.getInt("destino"),
             		   resultSet.getTime("tiempoTransito").toLocalTime(),
 					   resultSet.getFloat("capacidad"),
             		   resultSet.getBoolean("operativa")
             		   );
-            }
+            }*/
             
             resultSet.close();
             statement.close();
@@ -42,8 +42,8 @@ public class GestorRuta {
 			Connection connection = ConexionBDD.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement("insert into ruta(id, origen, destino, tiempoTransito, capacidad ,operativa) values(?, ?, ?, ?, ?, ?)");
 			preparedStatement.setInt(1, r.getId());
-			preparedStatement.setSucursal(2, r.getOrigen());
-			preparedStatement.setSucursal(3, r.getDestino());
+			//preparedStatement.setSucursal(2, r.getOrigen());
+			//preparedStatement.setSucursal(3, r.getDestino());
 			preparedStatement.setTime(4, Time.valueOf(r.getTiempoTransito()));
 			preparedStatement.setFloat(5, r.getCapacidad());
 			preparedStatement.setBoolean(6, r.isOperativa());
--- src/Gestores/GestorSucursal.java
+++ src/Gestores/GestorSucursal.java
@@ -1,9 +1,20 @@
 package Gestores;
 
 import java.sql.*;
+import java.time.LocalTime;
+
 import POJO.Sucursal;
 
 public class GestorSucursal {
+	
+	public static GestorSucursal gestor;
+	
+	public static GestorSucursal getInstance() {
+		if(gestor==null) {
+			gestor = new GestorSucursal();
+		}
+		return gestor;
+	}
 	
 	public Sucursal getSucursalById(int i) throws Exception{
 		Sucursal s = null;
@@ -36,9 +47,27 @@ public class GestorSucursal {
         
 	}
 	
-	public static void createSucursal(Sucursal s) throws Exception{
+
+	public Sucursal createSucursal(int id, String nombre, LocalTime horarioApertura, 
+			LocalTime horarioCierre, boolean operativa) {
+		
+		Sucursal s = new Sucursal(id,nombre,horarioApertura,horarioCierre,operativa);
+
+		try {
+		persistSucursal(s);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("No se ha podido persistir");
+		}
+		
+		return s;
+
+	}
+	
+	public void persistSucursal(Sucursal s) throws Exception{
 		
 		try {
+			System.out.println("Entré");
             // below two lines are used for connectivity.
 			Connection connection = ConexionBDD.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement("insert into sucursal(id, nombre, horarioApertura, horarioCierre, operativa) values(?, ?, ?, ?, ?)");
@@ -58,6 +87,7 @@ public class GestorSucursal {
         }
 		
 	}
+
 public static void deleteSucursal(int id) throws Exception{
 	
 	try {
@@ -74,4 +104,69 @@ public static void deleteSucursal(int id) throws Exception{
     }
 	
 }
+
+public Sucursal getSucursalByNombre(String nombre) throws Exception{
+	Sucursal s = null;
+    try {
+        // below two lines are used for connectivity.
+    	Connection connection = ConexionBDD.getConnection();
+        Statement statement;
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+            "select * from sucursal where nombre ="+nombre+"");
+   
+        while (resultSet.next()) {
+           s = new Sucursal(resultSet.getInt("sucursal_id"),
+        		   resultSet.getString("nombre"),
+        		   resultSet.getTime("horarioApertura").toLocalTime(),
+        		   resultSet.getTime("horarioCierre").toLocalTime(),
+        		   resultSet.getBoolean("operativa")
+        		   );
+        }
+        
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+    catch (Exception exception) {
+        System.out.println(exception);
+    }
+    
+    return s;
+    
+}
+
+public Sucursal getSucursal(int i, String nombre) throws Exception{
+	Sucursal s = null;
+    try {
+        // below two lines are used for connectivity.
+    	Connection connection = ConexionBDD.getConnection();
+        Statement statement;
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+            "select * from sucursal where sucursal_id ="+i+" and nombre="+nombre+"");
+   
+        while (resultSet.next()) {
+           s = new Sucursal(resultSet.getInt("sucursal_id"),
+        		   resultSet.getString("nombre"),
+        		   resultSet.getTime("horarioApertura").toLocalTime(),
+        		   resultSet.getTime("horarioCierre").toLocalTime(),
+        		   resultSet.getBoolean("operativa")
+        		   );
+        }
+        
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+    catch (Exception exception) {
+        System.out.println(exception);
+    }
+    
+    return s;
+    
+}
+
+
+
 }
