b261970977365df518db654f8df9bdc6c6bacc5f
JoaquinAleg <joaquin.alegre2000gmail.com>
2023-08-04 13:44:28 -0300

    Tocas el module info y el Joa me dispara.

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author tomid
+ *
+ */
+module TPDied {
+	requires java.desktop;
+	requires java.sql;
+	requires org.jgrapht.core;
+	requires org.jgrapht.ext;
+	requires com.github.vlsi.mxgraph.jgraphx;
+
+}
